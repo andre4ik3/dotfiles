d07906deafca8790450b0d4c3d756fd115c1cb5f
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-04-06 22:36:10 +0300

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# My solution to years of muscle memory
 if status is-login
     alias cat="bat"
     alias ls="exa"
@@ -5,11 +6,19 @@ if status is-login
     alias ps="procs"
     alias find="fd"
     alias sed="sd"
+    alias vim="edit"
 end
 
 starship init fish | source
 zoxide init fish | source
 
+export CHROME_EXECUTABLE=(which chromium)
+
+# Competions setup
+source /opt/homebrew/completions/fish/brew.fish
+
 jdk
 editor
+
+neofetch
 
